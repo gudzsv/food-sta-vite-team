55ed4581abf5fbef2532ef2d58e8a41146e8fcce
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-02-13 15:01:58 +0200

    change js modal

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,7 +1,29 @@
 const closeModalBtn = document.querySelector('[data-close-modal]');
 const backdrop = document.querySelector('[data-backdrop]');
-closeModalBtn.addEventListener('click', toogleModal);
 
-export default function toogleModal() {
-  backdrop.classList.toggle('is-open');
+closeModalBtn.addEventListener('click', onCloseModal);
+backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal(event) {
+  window.addEventListener('keydown', onEscPressKey);
+  backdrop.classList.add('is-open');
 }
+
+function onCloseModal(event) {
+  window.removeEventListener('keydown', onEscPressKey);
+  backdrop.classList.remove('is-open');
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onEscPressKey(event) {
+  if (event.code === 'Escape') {
+    onCloseModal();
+  }
+}
+
+export { onBackdropClick, onCloseModal, onEscPressKey, onOpenModal };
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import './js/header/header-mobile-menu';
 import './js/header/header-open-modal';
 import './js/hero/hero-open-modal';
 import './js/modal/modal-form';
+import './js/offering/offering-open-modal';
 import './js/subscribe/subscribe-form';
